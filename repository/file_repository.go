8aac02e8251b10dd69ae448f0641c3ad41769f60
Tamashoo <tamasho-tomoya-ch@ynu.jp>
2023-08-09 10:29:04 +0900

    user and file repository

---
+++ repository/file_repository.go
@@ -0,0 +1,50 @@
+package repository
+
+import (
+	"backend/model"
+
+	"gorm.io/gorm"
+)
+
+type IFileRepository interface {
+	GetAllFiles(files *[]model.File, userId uint) error
+	GetFileReviews(file *model.File, fileId uint) error
+	GetFileByUsername(files *[]model.File, username string) error
+	CreateFile(file *model.File) error
+}
+
+type fileRepository struct {
+	db *gorm.DB
+}
+
+func NewFileRepository(db *gorm.DB) IFileRepository {
+	return &fileRepository{db}
+}
+
+func (fr *fileRepository) GetAllFiles(files *[]model.File, userId uint) error {
+	if err := fr.db.Find(files).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (fr *fileRepository) GetFileReviews(file *model.File, fileId uint) error {
+	if err := fr.db.Joins("Eval").Where("file_id=?", fileId).First(file).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (fr *fileRepository) GetFileByUsername(files *[]model.File, username string) error {
+	if err := fr.db.Where("username = ?", username).Find(files).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (fr *fileRepository) CreateFile(file *model.File) error {
+	if err := fr.db.Create(file).Error; err != nil {
+		return err
+	}
+	return nil
+}
