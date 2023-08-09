de438ba8896bbc7a98bb9d3c6e277b22f57e3fce
Tamashoo <tamasho-tomoya-ch@ynu.jp>
2023-08-09 15:34:54 +0900

    fix codes

--- model/file.go
+++ model/file.go
@@ -5,20 +5,20 @@ import "time"
 type File struct {
 	FileId    uint      `json:"file_id" gorm:"primaryKey"`
 	Filename  string    `json:"filename" gorm:"not null"`
-	Fileurl   string    `json:"file_url" gorm:"not null"`
+	Fileurl   string    `json:"fileurl" gorm:"not null"`
 	CreatedAt time.Time `json:"created_at"`
 	Username  string    `json:"username"`
 }
 
 type FileResponse struct {
 	FileId    uint      `json:"id" gorm:"primaryKey"`
-	Filename  string    `json:"title" gorm:"not null"`
+	Filename  string    `json:"filename" gorm:"not null"`
 	Username  string    `json:"username"`
 	CreatedAt time.Time `json:"created_at"`
 } //一覧時に個数だけ送る
 
 type FileReviewResponse struct {
 	Result   bool           `json:"result"`
-	Fileurl  string         `json:"file_url"`
+	Fileurl  string         `json:"fileurl"`
 	Evallist []EvalResponse `json:"evallist"`
 } //評価ページに移動したときに送る
